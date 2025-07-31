471aa514cf02a9ca40ee0770ab29acc7bf3e722f
aaizaguirre <aaizaguirreo@gmail.com>
2025-07-31 15:15:38 -0500

    Implementación de función para sortear los amigos

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 let amigos = [];
 let amigo = document.getElementById("amigo");
 let lista = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
 
 //Función para agregar amigos 
 function agregarAmigo(){
@@ -16,10 +17,25 @@ function agregarAmigo(){
 
 // Función para actualizar la lista de amigos
 function actualizarAmigos(){
+    resultado.innerHTML = "";
     lista.innerHTML = "";
     for(let i=0; i < amigos.length; i++){
         let li = document.createElement("li");
         li.textContent = amigos[i];
         lista.appendChild(li);
+    };
+};
+
+function sortearAmigo(){
+    if (!amigos.length){
+        alert("No se insertó ningún nombre");
+    } else {
+        lista.innerHTML = "";
+        resultado.innerHTML = "";
+        let indice = Math.floor(Math.random()*amigos.length);
+        let mensaje = document.createElement("p");
+        mensaje.textContent = "El amigo secreto sorteado es: " + amigos[indice];
+        resultado.appendChild(mensaje);
+        amigos.length = 0;
     };
 };
