006c818723d243339727854e24b949d9ada520eb
aaizaguirre <aaizaguirreo@gmail.com>
2025-07-31 15:13:35 -0500

    Implementación de función para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // Creación de un array para almacenar los nombres
 let amigos = [];
+let amigo = document.getElementById("amigo");
+
+//Función para agregar amigos 
+function agregarAmigo(){
+    if (amigo.value.trim() === ""){
+        alert("Por favor, inserte un nombre");
+    } else {
+        amigos.push(amigo.value.trim());
+        amigo.value = "";
+    };
+};
