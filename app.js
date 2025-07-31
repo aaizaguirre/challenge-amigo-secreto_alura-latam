19ba49d8002ee7f87d1f6e5746c8929b7371d3cd
aaizaguirre <aaizaguirreo@gmail.com>
2025-07-31 15:14:58 -0500

    Implementación de función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Creación de un array para almacenar los nombres
 let amigos = [];
 let amigo = document.getElementById("amigo");
+let lista = document.getElementById("listaAmigos");
 
 //Función para agregar amigos 
 function agregarAmigo(){
@@ -9,5 +10,16 @@ function agregarAmigo(){
     } else {
         amigos.push(amigo.value.trim());
         amigo.value = "";
+        actualizarAmigos();
+    };
+};
+
+// Función para actualizar la lista de amigos
+function actualizarAmigos(){
+    lista.innerHTML = "";
+    for(let i=0; i < amigos.length; i++){
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
     };
 };
